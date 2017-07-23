6c8eff02c11dc34488ee0bd3cb49674705f52820
eukaryote <sapientdust+github@gmail.com>
2017-07-23 14:02:36 -0700

    fix classifiers in setup.py

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
     classifiers=[
         'Programming Language :: Python',
-        'Development Status :: 2 - Beta'
+        'Development Status :: 4 - Beta',
         'Natural Language :: English',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -76,7 +76,7 @@ setup(
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities'
+        'Topic :: Utilities',
     ],
     extras_require={
         'dev': ['check-manifest', 'wheel'],
